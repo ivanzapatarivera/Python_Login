e5923dff5b0605a804e0f279a6f03e66c8a626e5
Ivan Zapata <falenyo@gmail.com>
2021-04-03 18:07:49 -0500

    Created check password func

--- myproject/models.py
+++ myproject/models.py
@@ -17,3 +17,6 @@ class User(db.Model,UserMixin):
         self.username = username
         self.password_hash = generate_password_hash(password)
 
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+        
