6f4de1d60f1a5f1b3ee5332e67cd46700d785fc5
Ivan Zapata <falenyo@gmail.com>
2021-04-03 18:06:51 -0500

    Created User class

---
+++ myproject/models.py
@@ -0,0 +1,19 @@
+import werkzeug
+from myproject import db,login_manager
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+
+class User(db.Model,UserMixin):
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    email = db.Column(db.String(64), unique = True, index = True) ## 64 length to avoid spam
+    username = db.Column(db.String(64, unique = True, index = True)) 
+    password_hash = db.Column(db.String(128))
+
+    def __init__(self, email, username, passwrord): 
+        self.email = email
+        self.username = username
+        self.password_hash = generate_password_hash(password)
+
